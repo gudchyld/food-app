f4f514354e070244f883b30b155082aba02ed070
goodchild66@gmail.com <goodchild66@gmail.com>
2023-03-08 19:14:14 +0100

    added payout section

--- index.js
+++ index.js
@@ -2,6 +2,56 @@ import { menuArray } from "./data.js";
 
 const menu = document.getElementById('menu');
 
+document.addEventListener('click', (e)=> {
+  if (e.target.dataset.plus){
+  handlePlusClick(e.target.dataset.plus);
+  }
+})
+
+let menuArr = menuArrayWithCount()
+
+function handlePlusClick(id) {
+  let itemValue=``
+  let cumPrices = 0;
+  
+  menuArr.forEach(item => {
+    if (item.count > 0 && id == item.id ) {
+      item.count++
+      item.totalPrice += item.price
+      document.getElementById(`${id}`).textContent = `${item.totalPrice}`
+
+    }else if (id == item.id) {
+      item.count++
+      item.totalPrice += item.price
+      itemValue += `
+              <div class='payout-menu'>
+                <p>${item.name}</p>
+                <p class='pm-total' id='${item.id}'>${item.price}</p>
+                
+              </div>
+            `
+    }
+  })
+  
+  document.getElementById('payout-items').innerHTML += itemValue
+  
+  let tClass = document.querySelectorAll('.pm-total');
+  
+  for (let eclass of tClass) {
+    cumPrices += Number(eclass.textContent)
+  }
+  
+  document.getElementById('pt-amount').textContent = cumPrices;
+}
+
+function menuArrayWithCount(){
+  let newArr = menuArray.map(item => {
+    return {...item, 'count': 0, 'totalPrice': 0
+    }
+  } )
+  return newArr
+}
+
 function displayMenuItems(){
     let menuItems = ``
     menuArray.forEach(item => {
@@ -16,7 +66,7 @@ function displayMenuItems(){
                         <p class="menu-price">$${item.price}</p>
                     </div>
                     <div class="menu-icon">
-                    <i class="fa-solid fa-plus menu-add"></i>
+                    <i class="fa-solid fa-plus menu-add" data-plus='${item.id}'></i>
                     </div>
             </div>`
        
